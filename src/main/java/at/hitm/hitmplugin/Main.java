6f8ca2fa35592e15eabc6e0ab1f6680336e9c4b4
SIMULATAN <simulatan2319@gmail.com>
2021-09-07 14:00:24 +0200

    Fix merge mistakes

--- src/main/java/at/hitm/hitmplugin/Main.java
+++ src/main/java/at/hitm/hitmplugin/Main.java
@@ -6,35 +6,30 @@
 package at.hitm.hitmplugin;
 
 import at.hitm.hitmplugin.api.API;
+import at.hitm.hitmplugin.commands.DateCommand;
 import at.hitm.hitmplugin.commands.GetOnlinePlayersCommand;
 import at.hitm.hitmplugin.commands.GiveMoneyCommand;
 import at.hitm.hitmplugin.listeners.APIListener;
 import at.hitm.hitmplugin.listeners.JoinLeaveListener;
-import at.hitm.hitmplugin.utils.PerformanceMonitor;
 import at.hitm.hitmplugin.utils.TPSRunnable;
 import at.hitm.hitmplugin.items.ItemManager;
-import at.hitm.hitmplugin.listeners.JoinLeaveListener;
 import at.hitm.hitmplugin.listeners.PlayerCraftListener;
 import at.hitm.hitmplugin.utils.Utils;
 import at.hitm.hitmplugin.warps.WarpCommand;
 import at.hitm.hitmplugin.warps.WarpManager;
+import at.hitm.hitmplugin.warps.WarpsCommand;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.PluginCommand;
-import org.bukkit.command.TabCompleter;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.Objects;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public final class Main extends JavaPlugin {
 
@@ -88,7 +83,6 @@ public final class Main extends JavaPlugin {
         getCommand("date").setExecutor(new DateCommand());
         getCommand("getOnlinePlayers").setExecutor(new GetOnlinePlayersCommand());
         getCommand("giveMoney").setExecutor(new GiveMoneyCommand());
-        getCommand("date").setExecutor(new DateCommand());
         getCommand("getOnlinePlayers").setExecutor(new GetOnlinePlayersCommand());
         getCommand("giveMoney").setExecutor(new GiveMoneyCommand());
         // used to setup warps
@@ -109,10 +103,6 @@ public final class Main extends JavaPlugin {
             if (args.length <= 1) return Utils.getElementsStartingWith(new ArrayList<>(WarpManager.warps.keySet()), args[0].toLowerCase());
             return null;
         });
-    }
-
-    public static Main getInstance() {
-        return instance;
     }
 
     public static void send(CommandSender sender, String message) {
